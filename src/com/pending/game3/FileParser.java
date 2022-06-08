4514f2b8b2b896ef5c6c0ae32e5988ed10969204
Justin Bruce <102606341+justinbruce09@users.noreply.github.com>
2022-06-08 14:31:43 -0500

    created classes to avoid merge conflicts

---
+++ src/com/pending/game3/FileParser.java
@@ -0,0 +1,13 @@
+package com.pending.game3;
+
+class FileParser {
+        String startingRoom;
+        String startingInventory[];
+        Room roomsAtStart[];
+        Item itemsAtStart[];
+        NPC npcsAtStart[];
+        CraftingRecipe recipes[];
+        EndCondition endCondition[];
+
+
+}

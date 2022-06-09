03f7309143a0c3f50fb96a3439980ce6e44ceeff
Justin Bruce <102606341+justinbruce09@users.noreply.github.com>
2022-06-09 15:43:00 -0500

    Text parser created as getInput in InputParser, AB#106

--- src/com/pending/game3/InputParser.java
+++ src/com/pending/game3/InputParser.java
@@ -1,5 +1,15 @@
 package com.pending.game3;
 
+import java.util.Scanner;
+
 class InputParser {
+    //create a scanner object
+//    Scanner userInput = new Scanner(System.in);
+    // getInput()
+    String getInput(Scanner userInput){
+        System.out.print("Enter Command\n> "); // allows input command to be on next line
+        String input = userInput.nextLine(); // Reads user input
+        return null;
+    }
 
 }

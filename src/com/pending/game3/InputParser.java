d265cff67446655718496fcea715d8393675c3f6
Justin Bruce <102606341+justinbruce09@users.noreply.github.com>
2022-06-10 16:48:21 -0500

    Input Parser created with look and take, AB#103, AB#106, AB#107, AB#108, AB#110, AB#111

--- src/com/pending/game3/InputParser.java
+++ src/com/pending/game3/InputParser.java
@@ -1,39 +1,60 @@
 package com.pending.game3;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 class InputParser {
     //create a scanner object
 //    Scanner userInput = new Scanner(System.in);
     // getInput()
-    void getInput(Scanner userInput){
+    boolean getInput(Scanner userInput){
         System.out.print("Enter Command\n> "); // allows input command to be on next line
-        String input = userInput.nextLine(); // Reads user input
+        String input = userInput.nextLine().toLowerCase(); // Reads user input
 
-        String[] inputSplit = input.split(" ", 2);
-        System.out.println(inputSplit[0] + "," + inputSplit[1]);
-        SynonymDictionary command;
-//        try{
-//            command = SynonymDictionary.valueOf(inputSplit[0]);
-//        } catch (Exception e){
-//            if (SynonymDictionary.GO.synonmys.contains(inputSplit[0])) {
-//                command = SynonymDictionary.GO;
-//            } //else if (check the rest of SynonymDict for matches)
-//            else {
-//                System.out.println("command " + input +
-//                        " not recognized. enter \"Help\" for a list of valid commands.");
-//            }
-//        }
-//        switch (command){
+        String[] inputSplit = input.split(" ", 2); // splits array in 2 after 1st space
+        System.out.println(inputSplit[0] + "," + inputSplit[1]); // everything after space is element 1
+        SynonymDictionary command = null;
+        try{
+            command = SynonymDictionary.valueOf(inputSplit[0]); //command = verb
+        } catch (Exception e){
+            if (SynonymDictionary.GO.synonyms.contains(inputSplit[0])) { //if a synonym of "go" in syn dict use go
+                command = SynonymDictionary.GO;
+            } //else if (check the rest of SynonymDict for matches)
+            else {
+                System.out.println("command " + input +
+                        " not recognized. enter \"Info\" for a list of valid commands.");
+                return false;
+            }
+        }
+        switch (command){
 //            case GO:
 //                //Check inputSplit[1] against available directions, then get that destination room
 //                //goToRoom(destination);
+//                if (destinations.contains.key (inputSplit[1]){
+//                    goToRoom(destinations);
+//                }
 //                break;
-//
-//
-//            default:
-//                System.out.println("Command not yet supported");
-//        }
+            case INSPECT:
+                if("room".equalsIgnoreCase(inputSplit[1])){
+                System.out.println(Game3.getCurrentRoom().description);
+                }
+                    if (Game3.getCurrentRoom().items.contains(inputSplit[1])) {
+                        System.out.println(Game3.getItems().get(inputSplit[1]));
+                    } else {
+                        System.out.println("Item does not exist");
+                    }
+                break;
+            case TAKE:
+                    if (Game3.getCurrentRoom().items.contains(inputSplit[1])) {
+                    Game3.getCurrentRoom().takeItem(inputSplit[1]);
+                    }
+                break;
+            case QUIT:
+                return true;
+            default:
+                System.out.println("Command not yet supported");
+       }
+                return false;
     }
 
     private boolean goToRoom(Room destination) {

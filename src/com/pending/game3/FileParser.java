ead4899244372bce63aa63f3b264c7c5317174de
agentronin316 <agentronin316@users.noreply.github.com>
2022-06-13 16:54:26 -0500

    AB#113

--- src/com/pending/game3/FileParser.java
+++ src/com/pending/game3/FileParser.java
@@ -1,5 +1,6 @@
 package com.pending.game3;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
@@ -7,7 +8,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.List;
 
@@ -29,25 +29,28 @@ class FileParser {
         static FileParser loadFile(Path test1) throws FileNotFoundException {
                 FileParser toReturn = new FileParser();
                 // created parser
-//                Object obj = JSONValue.parse(new FileReader(String.valueOf(test1)));
-//                jsonObject = (JSONObject) obj;
+                Object obj = JSONValue.parse(new FileReader(String.valueOf(test1)));
+                if (JSONObject.class == obj.getClass()) {
+                        jsonObject = (JSONObject) obj;
+                        if (toReturn.parseRooms()) return null;
+                }
 
                 //Dummy logic for Iteration 1 MVP, will replace with actual JSON parsing in Iteration 2
                 toReturn.startingInventory = new ArrayList<>();
-                toReturn.roomsAtStart = new HashMap<>();
-                Room room = new Room();
-                room.description = "What appears to be an examination room with three walls, one of which " +
-                                   "is curved outward and has a door in it.";
-                room.items = new ArrayList<>();
-                room.items.add("body-scanning device");
-                room.items.add("medicine");
-                room.items.add("surgical instrument trolley");
-                room.items.add("key card");
-                room.flags = new ArrayList<>();
-                room.npcs = new ArrayList<>();
-                room.name = "examination room";
-                room.displayName = "examination room";
-                toReturn.roomsAtStart.put(room.name, room);
+//                toReturn.roomsAtStart = new HashMap<>();
+//                Room room = new Room();
+//                room.description = "What appears to be an examination room with three walls, one of which " +
+//                                   "is curved outward and has a door in it.";
+//                room.items = new ArrayList<>();
+//                room.items.add("body-scanning device");
+//                room.items.add("medicine");
+//                room.items.add("surgical instrument trolley");
+//                room.items.add("key card");
+//                room.flags = new ArrayList<>();
+//                room.npcs = new ArrayList<>();
+//                room.name = "examination room";
+//                room.displayName = "examination room";
+//                toReturn.roomsAtStart.put(room.name, room);
                 toReturn.startingRoom = "examination room";
                 toReturn.itemsAtStart = new HashMap<>();
                 Item item = new Item();
@@ -84,6 +87,157 @@ class FileParser {
                 toReturn.recipes = new ArrayList<>();
                 toReturn.endConditions = new ArrayList<>();
 
+                return toReturn;
+        }
+
+        private boolean parseRooms() {
+                roomsAtStart = new HashMap<>();
+                Object obj = jsonObject.get("Rooms");
+                if (JSONArray.class == obj.getClass()) {
+                        JSONArray jsonRooms = (JSONArray) obj;
+                        for (Object roomObj : jsonRooms) {
+                                //programmer.tryNotToCry();
+                                //programmer.cryALot();
+                                JSONObject roomJsonObj = (JSONObject) roomObj;
+                                Room room = new Room();
+
+                                room.name = parseString(roomJsonObj.get("Name"));
+                                if (room.name == null) {
+                                        System.out.println("Room Name.");
+                                        return true;
+                                }
+
+                                if (roomJsonObj.keySet().contains("Effect Tags")) {
+                                        obj = roomJsonObj.get("Effect Tags");
+                                        if (JSONArray.class.equals(obj.getClass())) {
+                                                JSONArray flagsJson = (JSONArray) obj;
+                                                room.flags = parseFlags(flagsJson);
+                                                if (room.flags == null) {
+                                                        System.out.println(room.name + " Effect Tags.");
+                                                        return true;
+                                                }
+                                        } else return true;
+                                } else room.flags = new ArrayList<>();
+
+                                if (roomJsonObj.keySet().contains("Items")) {
+                                        room.items = parseStringList(roomJsonObj.get("Items"));
+                                        if (room.items == null) {
+                                                System.out.println("Room " + room.name + " Items.");
+                                                return true;
+                                        }
+                                } else room.items = new ArrayList<>();
+
+                                //parse connected rooms
+                                obj = roomJsonObj.get("Connected Rooms");
+                                room.connections = new HashMap<>();
+                                if(JSONArray.class.equals(obj.getClass())){
+                                        JSONArray connectionArray = (JSONArray)obj;
+                                        for (Object derp : connectionArray) {
+                                                if (JSONObject.class.equals(derp.getClass())){
+                                                        JSONObject connection = (JSONObject)derp;
+                                                        if(connection.keySet().size() == 1){
+                                                                obj = connection.keySet().toArray()[0];
+                                                                String direction = parseString(obj);
+                                                                if (direction == null){
+                                                                        System.out.println("Room " + room.name +
+                                                                                " Connection Direction.");
+                                                                        return true;
+                                                                }
+                                                                obj = connection.get(direction);
+                                                                String destination = parseString(obj);
+                                                                if (destination == null) {
+                                                                        System.out.println("Room " + room.name +
+                                                                                " Connection Destination.");
+                                                                return true;
+                                                        }
+                                                                room.connections.put(direction, destination);
+                                                        } else {
+                                                                System.out.println("Invalid Room " + room.name +
+                                                                        " Connection Object " +
+                                                                        "Size, each direction:destination pair must " +
+                                                                        "be a separate object and cannot be empty.");
+                                                                return true;
+                                                        }
+                                                } else {
+                                                        System.out.println("Invalid Data Type when Object was " +
+                                                                "expected at Room " + room.name + " Connections");
+                                                        return true;
+                                                }
+                                        }
+                                }
+
+                                room.description = parseString(roomJsonObj.get("Description"));
+                                if (room.description == null) {
+                                        System.out.println("Room " + room.name + " Description.");
+                                        return true;
+                                }
+
+                                roomsAtStart.put(room.name, room);
+
+                        }
+                }
+                return false;
+        }
+
+        private String parseString(Object obj) {
+
+                if (obj == null) {
+                        System.out.print("Null when String was expected at: ");
+                        return null;
+                }
+                if (String.class.equals(obj.getClass())){
+                        return (String)obj;
+                } else {
+                        System.out.print("Invalid Data Type when String was expected at: ");
+                        return null;
+                }
+        }
+
+        private List<Flag> parseFlags(JSONArray flagsJson) {
+                List<Flag> toReturn = new ArrayList<>();
+                if (flagsJson == null) {
+                        System.out.print("Null when Flags were expected at: ");
+                        return null;
+                }
+                for (Object loopObj : flagsJson) {
+                        if (JSONObject.class == loopObj.getClass()){
+                                JSONObject flagJson = (JSONObject) loopObj;
+                                String name = parseString(flagJson.get("Tag Name"));
+                                if (name == null) {
+                                        System.out.print("Tag Name in: ");
+                                        return null;
+                                }
+                                List<String> dataList = parseStringList(flagJson.get("Tag Data"));
+                                if(dataList == null) {
+                                        System.out.print("Tag Data in: ");
+                                        return null;
+                                }
+
+                        } else return null;
+                }
+                return toReturn;
+        }
+
+        private List<String> parseStringList(Object input) {
+                List<String> toReturn = new ArrayList<>();
+                if (input == null) {
+                        System.out.print("Null when Array of String was expected at: ");
+                        return null;
+                }
+                if(input.getClass().equals(JSONArray.class)) {
+                        JSONArray data = (JSONArray) input;
+                        for(Object obj : data) {
+                                String string = parseString(obj);
+                                if (string == null) {
+                                        System.out.print("Array of String in: ");
+                                        return null;
+                                }
+                                else toReturn.add(string);
+                        }
+                }else {
+                        System.out.print("Incorrect Data Type when Array of String was expected at: ");
+                        return null;
+                }
                 return toReturn;
         }
 }
--- src/com/pending/game3/Game3.java
+++ src/com/pending/game3/Game3.java
@@ -11,7 +11,7 @@ import java.util.stream.Stream;
 
 class Game3 {
     public static final String jsonDir = "resources/json";
-    public static final String mainSplash = "Insert Splash\nScreen Graphic\nHere!";
+    public static final String mainSplash = "Insert Splash\nScreen Graphic\nHere!\nType \"quit\" to exit";
     private FileParser fileParser;
     private InputParser inputParser;
     private List<String> inventory;
@@ -83,7 +83,6 @@ class Game3 {
         System.out.println(mainSplash);
         try (Stream<Path> stream = Files.list(Path.of(jsonDir))) {
             List<Path> files = getJsonList(stream);
-//            printFiles(files);
             if (promptUserForFile(reader, files)) return;
         } catch (Exception e) {
             System.out.println("Unable to locate resources\\json folder.");
@@ -115,26 +114,27 @@ class Game3 {
     private boolean promptUserForFile(Scanner reader, List<Path> files) {
 
         // Dummy code for Iteration 1, delete when ready to uncomment real code below
-        try {
-            fileParser = FileParser.loadFile(Path.of(jsonDir));
-        } catch (Exception e) {
-            System.out.println("FileParser error");
-        }
+//        try {
+//            fileParser = FileParser.loadFile(Path.of(jsonDir));
+//        } catch (Exception e) {
+//            System.out.println("FileParser error");
+//        }
 
         //Real code for files, uncomment when ready
-//        while (true){
-//            System.out.print("Enter a number to select a json file to load: ");
-//            String input = reader.nextLine();
-//            if ("quit".equals(input.toLowerCase())) return true;
-//            try{
-//                int inputIndex = Integer.parseInt(input) - 1;
-//                fileParser = FileParser.loadFile(files.get(inputIndex));
-//                break;
-//            } catch (Exception e) {
-//                System.out.println("Invalid input, please try again.");
-//            }
-//        }
-        return false;
+        while (true){
+            printFiles(files);
+            System.out.print("Enter a number to select a json file to load: ");
+            String input = reader.nextLine();
+            if ("quit".equals(input.toLowerCase())) return true;
+            try{
+                int inputIndex = Integer.parseInt(input) - 1;
+                fileParser = FileParser.loadFile(files.get(inputIndex));
+                if(fileParser == null) return true;
+                else return false;
+            } catch (Exception e) {
+                System.out.println("Invalid input, please try again.");
+            }
+        }
     }
 
     private void printFiles(List<Path> files) {

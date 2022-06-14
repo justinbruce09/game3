b9357de185c3a28bf5b312873a4816380f49163d
agentronin316 <agentronin316@users.noreply.github.com>
2022-06-14 15:39:15 -0500

    Implement GO command, AB#109

--- src/com/pending/game3/InputParser.java
+++ src/com/pending/game3/InputParser.java
@@ -41,13 +41,9 @@ class InputParser {
             case GO:
                 HashMap<String,  String> connectionsMap = Game3.getCurrentRoom().connections;
                 if (connectionsMap.containsKey(inputSplit[1])) {
-                    if(goToRoom(connectionsMap.get(inputSplit[1]))) {
-
-                    }else{
-                        // error message Room Not Found.
-                    }
+                    goToRoom(connectionsMap.get(inputSplit[1]));
                 } else {
-                    System.out.println();
+                    System.out.println("There's nowhere to go that direction.");
                 }
                 break;
             case INSPECT:
@@ -76,17 +72,18 @@ class InputParser {
                 return false;
     }
 
-    private boolean goToRoom(String destination) {
+    private void goToRoom(String destination) {
         if (Game3.getRooms().containsKey(destination)) {
             Room destinationRoom = Game3.getRooms().get(destination);
             if (destinationRoom.flags.containsKey("Locked")) {
-                //check for matching key
+                //get the lock and check inventory for matching key
                 List<String> keyData = destinationRoom.flags.get("Locked");
                 if (keyData.size() > 0){
-                    // we can loop over the data to check for multiple keys, or just restrict it to one key
-
                     if(keyCheck(keyData.get(0))){
-
+                        destinationRoom.flags.remove("Locked");
+                        Game3.setCurrentRoom(destinationRoom);
+                    } else {
+                        System.out.println("The door is locked.");
                     }
                 }else{
                     System.out.println("Locked flag on Room " + destination +
@@ -95,19 +92,20 @@ class InputParser {
                 //then remove "Locked" flag, move to new room, and return true;
             } else { //move to destination room and confirm move completed.
                 Game3.setCurrentRoom(destinationRoom);
-                return true;
             }
+        } else {
+            System.out.println("Room " + destination + " does not exist.");
         }
-        return false;
     }
 
     private boolean keyCheck(String keyId) {
         for (String itemName : Game3.getInventory()){
             if(Game3.getItems().containsKey(itemName)){
                 Item item = Game3.getItems().get(itemName);
-//                if(item.flags.containsKey("Key")){
-//                    //TODO: grab the "Key" flag, and check if the data matches keyId
-//                }
+                HashMap<String, List<String>> itemFlags = item.getFlags();
+                if(itemFlags.containsKey("Key") && itemFlags.get("Key").contains(keyId)){
+                    return true;
+                }
             }
         }
         return false;
